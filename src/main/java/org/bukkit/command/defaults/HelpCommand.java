1ca8d26b406da51d98b1570dd54b994d0e578e36
Satoshi Arai(Lab) <st.arai0227@gmail.com>
2013-07-12 11:55:32 +0900

    Add if block not to occur NPexception.

--- src/main/java/org/bukkit/command/defaults/HelpCommand.java
+++ src/main/java/org/bukkit/command/defaults/HelpCommand.java
@@ -197,48 +197,50 @@ public class HelpCommand extends VanillaCommand {
 		if (s1 == null && s2 != null) {
 			return s2.length();
 		}
+		if (s1 != null && s2 != null) {
+			int s1Len = s1.length();
+			int s2Len = s2.length();
+			int[][] H = new int[s1Len + 2][s2Len + 2];
 
-		int s1Len = s1.length();
-		int s2Len = s2.length();
-		int[][] H = new int[s1Len + 2][s2Len + 2];
-
-		int INF = s1Len + s2Len;
-		H[0][0] = INF;
-		for (int i = 0; i <= s1Len; i++) {
-			H[i + 1][1] = i;
-			H[i + 1][0] = INF;
-		}
-		for (int j = 0; j <= s2Len; j++) {
-			H[1][j + 1] = j;
-			H[0][j + 1] = INF;
-		}
-
-		Map<Character, Integer> sd = new HashMap<Character, Integer>();
-		for (char Letter : (s1 + s2).toCharArray()) {
-			if (!sd.containsKey(Letter)) {
-				sd.put(Letter, 0);
+			int INF = s1Len + s2Len;
+			H[0][0] = INF;
+			for (int i = 0; i <= s1Len; i++) {
+				H[i + 1][1] = i;
+				H[i + 1][0] = INF;
 			}
-		}
+			for (int j = 0; j <= s2Len; j++) {
+				H[1][j + 1] = j;
+				H[0][j + 1] = INF;
+			}
 
-		for (int i = 1; i <= s1Len; i++) {
-			int DB = 0;
-			for (int j = 1; j <= s2Len; j++) {
-				int i1 = sd.get(s2.charAt(j - 1));
-				int j1 = DB;
-
-				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-					H[i + 1][j + 1] = H[i][j];
-					DB = j;
-				} else {
-					H[i + 1][j + 1] = Math.min(H[i][j], Math.min(H[i + 1][j], H[i][j + 1])) + 1;
+			Map<Character, Integer> sd = new HashMap<Character, Integer>();
+			for (char Letter : (s1 + s2).toCharArray()) {
+				if (!sd.containsKey(Letter)) {
+					sd.put(Letter, 0);
 				}
-
-				H[i + 1][j + 1] = Math.min(H[i + 1][j + 1], H[i1][j1] + (i - i1 - 1) + 1
-						+ (j - j1 - 1));
 			}
-			sd.put(s1.charAt(i - 1), i);
-		}
 
-		return H[s1Len + 1][s2Len + 1];
+			for (int i = 1; i <= s1Len; i++) {
+				int DB = 0;
+				for (int j = 1; j <= s2Len; j++) {
+					int i1 = sd.get(s2.charAt(j - 1));
+					int j1 = DB;
+
+					if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+						H[i + 1][j + 1] = H[i][j];
+						DB = j;
+					} else {
+						H[i + 1][j + 1] = Math.min(H[i][j], Math.min(H[i + 1][j], H[i][j + 1])) + 1;
+					}
+
+					H[i + 1][j + 1] = Math.min(H[i + 1][j + 1], H[i1][j1] + (i - i1 - 1) + 1
+							+ (j - j1 - 1));
+				}
+				sd.put(s1.charAt(i - 1), i);
+			}
+
+			return H[s1Len + 1][s2Len + 1];
+		}
+		return -1;
 	}
 }
